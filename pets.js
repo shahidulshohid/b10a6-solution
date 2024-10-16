47af87440017c5ce5623edcd3a519b1a0d858226
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-16 17:44:10 +0600

    some project is done

--- pets.js
+++ pets.js
@@ -7,17 +7,52 @@ const loadCategories = async() => {
 
 //load all pets
 const loadAllPets = async() => {
-  const res = fetch(``)
+  const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
+  const data = await res.json()
+  displayPets(data.pets)
+}
+
+//load pets by category
+const loadPetsByCategory = async(category) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+  const data = await res.json()
+  displayPets(data.data)
+}
+
+//display all pets
+const displayPets = (pets) => {
+  const petContainers = document.getElementById('all-pets')
+  petContainers.innerHTML = ""
+  pets.forEach(pet => {
+    const div = document.createElement('div')
+    div.classList.add("flex", "flex-col", "gap-2", "p-4", "border", "rounded-xl", "font-bold",)
+    div.innerHTML = `
+    <img class="h-36 w-full rounded-xl object-cover" src="${pet.image}"/>
+    <h3 class="text-xl">${pet.pet_name}</h3>
+    <p class="text-sm text-gray-700">Breed: ${pet.breed ? pet.breed : 'Not Available'}</p>
+    <p class="text-sm text-gray-700">Birth: ${pet.date_of_birth ? pet.date_of_birth : 'Not Available'}</p>
+    <p class="text-sm text-gray-700">Gender: ${pet.gender ? pet.gender : 'Not Available'}</p>
+    <p class="text-sm text-gray-700">Price:  ${pet.price ? "$" + pet.price : 'Not Available'}</p>
+    <hr class="my-2"/>
+    <div class="flex justify-between items-center px-2">
+    <button class="btn bg-white text-teat-700 border rounded-lg py-1 px-4">
+    <i class="fa-regular fa-thumbs-up"></i>
+    </button>
+    <button class="btn bg-white text-teat-700 border rounded-lg py-1 px-4">Adopt</button>
+    <button class="btn bg-white text-teat-700 border rounded-lg py-1 px-4">Details</button>
+    </div>
+    `;
+    petContainers.appendChild(div)
+  })
 }
 
 //display categories
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById('pet-categories')
   categories.forEach((category) =>{
-    console.log(category)
     const div = document.createElement('div')
     div.innerHTML = `
-    <button class="btn category-btn bg-white flex items-center gap-4 rounded-xl border px-14 py-4 cursor-pointer h-full">
+    <button onclick="loadPetsByCategory('${category.category}')" class="btn category-btn bg-white flex items-center gap-4 rounded-xl border px-14 py-4 cursor-pointer h-full">
     <img class="w-10" src="${category.category_icon}"/>
     <p class="text-xl font-bold">${category.category}</p>
     </button>
@@ -26,4 +61,5 @@ const displayCategories = (categories) => {
   })
 }
 
+loadAllPets()
 loadCategories()
